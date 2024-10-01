6b9f36b7525e36bcba5d08b709171e9a413c9e85
Бачурин Николай Владимирович <101479452+rox0-0@users.noreply.github.com>
2024-10-02 01:21:13 +0300

    вроде всё

--- lab1/task10/task10.c
+++ lab1/task10/task10.c
@@ -6,9 +6,7 @@
 #include <math.h> 
 #include <ctype.h> 
 #define ll long int; 
-//sscanf - проверка целочисленного введенного числа 
-//if (sscanf("-123.45", "%lf", &d)==1) то корректно иначе нет; 
-// %g экспоненциальный ввод числа %e 
+
 typedef enum  
 { 
     OK, 
@@ -18,7 +16,7 @@ typedef enum
     ANY
 } status; 
  
-status checking_imput(char * s, int base)
+status checking_input(const char * s, const int base)
 {
     if (!s) return MEMORY_ERROR;
     int i = 0;
@@ -28,12 +26,13 @@ status checking_imput(char * s, int base)
         i++;
     }
 
+    if ((strlen(s)>2 && s[0]=='-' && s[1]=='0')|| (strlen(s)>1 && s[0]=='0')) return WRONG_INPUT_ERROR;
     
-    if (base>=10)
+    if (base>10)
     {
         while(s[i]!='\0')
         {
-            if (s[i] - 'a' + 10 >= base || (s[i]<='Z' && s[i] >= 'A')) return WRONG_INPUT_ERROR;
+            if (s[i] - 'a' + 10 >= base || (s[i]<='Z' && s[i] >= 'A')||((s[i]>'z' || s[i] < 'a') && !(s[i]>='0' && s[i]<='9')) ) return WRONG_INPUT_ERROR;
             i++;
         }
     }
@@ -41,7 +40,7 @@ status checking_imput(char * s, int base)
     {
         while (s[i]!='\0')
         {
-            if (s[i]-'0'>=base) return WRONG_INPUT_ERROR;
+            if (s[i]-'0'>=base||(s[i]<'0' || s[i]>'9')) return WRONG_INPUT_ERROR;
             i++;
             
         }
@@ -49,11 +48,11 @@ status checking_imput(char * s, int base)
     return OK;
 } 
 
-long long int base_2_ten(char * num, int base)
+status base_2_ten(char * num, int base, int * answer)
 {
-    long long int res = 0;
+    int res = 0;
     int size = strlen(num);
-    long long int power = 1;
+     int power = 1;
     int sign = 1;
     if(num[0] == '-') sign = -1;
     for(int i = size - 1;  i>=0; i--){
@@ -61,52 +60,83 @@ long long int base_2_ten(char * num, int base)
             break;
         }
         if(isdigit(num[i])){
+            if (res>INT_MAX - (num[i] - '0') * power || res < INT_MIN + (num[i] - '0') * power) return MEMORY_ERROR;
             res += (num[i] - '0') * power;
         }
         if(isalpha(num[i])){
+            if (res>INT_MAX - (tolower(num[i]) - 'a' + 10) * power|| res < INT_MIN +(tolower(num[i]) - 'a' + 10) * power) return MEMORY_ERROR;
+
             res += (tolower(num[i]) - 'a' + 10) * power;
         }
         power *= base;
     }
     res *=sign;
-    return res;
+    *answer = res;
+    return OK;
 }
 
 status ten_2_base(int num, int base, char  * res[])
 {
+    if (num == 0)
+    {
+        char * mas = (char *)malloc(2*sizeof(char));
+        if (res== NULL) return MEMORY_ERROR;
+        mas[0] = '0';
+        mas[1]='\0';
+        *res = mas;
+        return OK;
+    }
+    else 
+    {
     int sign =1;
     if (num<0) sign = -1;
     int b = num*sign;
     int k=0;
-    int a = num*sign;
+    int a = b;
     while (a>0){a /= base; k++;}
     if (sign==-1) k++;
-    *res=malloc(sizeof(char)*(k+1));
-    (*res)[k]='\0';
-    if (*res)
+
+
+    char * mas =(char *) malloc(sizeof(char)*(k+1));
+    
+    
+    if (mas)
     {
+    mas[k]='\0';
     int i=0;
-    if (sign==-1){(*res)[0]='-'; i++;}
-    while(i<k)
+    if (sign==-1){mas[0]='-';}
+    while(i<k && b>0)
     {
-        if (b%base<=9) (*res)[i]=b%base + '0';
-        else (*res)[i]=b%base + 'a'-10;
+        if (b%base<10) mas[k-i-1]=b%base + '0';
+        else mas[k-i-1]=b%base + 87;
         b/=base;
         i++;
     }
+    *res = mas;
     return OK;
     }
-    else return MEMORY_ERROR;
-}
+
+    else
+    {
+        
+        return MEMORY_ERROR;
+    }
+    }
+}   
 
 
 int main(int argc, char*argv[]) 
 { 
     int base;
     scanf("%d", &base);
-    char  s[100];
-    if (base<2 || base>36) return WRONG_INPUT_ERROR;
+    char  s[BUFSIZ];
+    if (base<2 || base>36)
+    {
+        printf("неправильное основание \n");
+        return WRONG_INPUT_ERROR;
+    }
     int max = INT_MIN;
+    int ans;
     while(1) 
     {
         scanf("%s", s);
@@ -114,18 +144,42 @@ int main(int argc, char*argv[])
         {
             break;
         }
-        if (checking_imput(s, base)==WRONG_INPUT_ERROR) return WRONG_INPUT_ERROR;
-        max = abs(base_2_ten(s, base))>abs(max)? base_2_ten(s, base):max;
+        if (checking_input(s, base)==WRONG_INPUT_ERROR){printf("вронг инпут\n"); return WRONG_INPUT_ERROR;}
+        if (base_2_ten(s, base, &ans)==OK)
+        max = abs(ans)>abs(max)? ans:max;
+        else {printf("ошибка с памятью\n"); return MEMORY_ERROR;}
     }
-    printf("%d\n", max);
+
+    
+    
     char * res;
+    if (ten_2_base(max, base, &res) == OK) 
+    {
+        
+        printf("%s\n", res);
+        free(res);
+    }
+
     if (ten_2_base(max, 9, &res) == OK) 
     {
         
-        printf("%s", res);
+        printf("%s\n", res);
         free(res);
     }
-    else if(ten_2_base(max,9,&res)==MEMORY_ERROR) return MEMORY_ERROR;
+    if (ten_2_base(max, 18, &res) == OK) 
+    {
+        
+        printf("%s\n", res);
+        free(res);
+    }
+    if (ten_2_base(max, 36, &res) == OK) 
+    {
+        
+        printf("%s\n", res);
+        free(res);
+    }
+
+    else if(ten_2_base(max,16,&res)==MEMORY_ERROR){ printf("многа букф\n");return MEMORY_ERROR;}
     return OK;
     
 }
