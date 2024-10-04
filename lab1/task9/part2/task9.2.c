d7bbad105e9adf7e2f94f303f08e5b67c69a8891
Бачурин Николай Владимирович <101479452+rox0-0@users.noreply.github.com>
2024-10-05 01:40:40 +0300

    Update task9.2.c

--- lab1/task9/part2/task9.2.c
+++ lab1/task9/part2/task9.2.c
@@ -1,34 +1,105 @@
-#include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h> 
-#include <stdbool.h> 
-#include <limits.h> 
-#include <math.h> 
-#include <ctype.h> 
-
-typedef enum  
-{ 
-    OK, 
-    WRONG_VALUE_ERROR, 
-    MEMORY_ERROR, 
-    WRONG_INPUT_ERROR,
-    ANY
-} status; 
-
-int main(int argc, char* argv[])
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <math.h>
+#include <ctype.h>
+#define ll long int;
+#define PI 3.14159265358979323846
+typedef enum 
 {
-    int a,b;
-    int N =  rand() % (10000 - 10 + 1)+10 ;
-    int * array = malloc(sizeof(int)*N);
+    OK,
+    WRONG_VALUE_ERROR,
+    MEMORY_ERROR,
+    WRONG_INPUT_ERROR
+} status;
+
+int compare(const void * x1, const void * x2)   
+{
+  return ( *(int*)x1 - *(int*)x2 );            
+}
+int main()
+{
+    int * a;
+    int * b;
+    int * c;
+    int a_size = rand()%(10001 - 10) + 10 ;
+    int b_size = rand()%(10001 - 10) + 10 ;
     
-    //srand(time(NULL));
+    a = malloc(sizeof (int)*(a_size));
+    if (a == NULL)
+    {
+        printf("ошибка памяти\n");
+        return MEMORY_ERROR;
+
+    }
+    b = malloc(sizeof (int)*(b_size));
+     if (b == NULL)
+    {
+        printf("ошибка памяти\n");
+        return MEMORY_ERROR;
+        
+    }
     
-    for (int i=0; i<N; ++i)
-    array[i] = rand() % (1000 + 1000 + 1) - 1000;
+    c = malloc(sizeof(int) * (a_size));
+    if (c == NULL)
+    {
+        printf("ошибка памяти\n");
+        return MEMORY_ERROR;
+        
+    }
+
+    for (int i = 0; i< a_size; ++i)
+    {
+        a[i] = rand()%2001 - 1000;
     
-    for (int i=0; i<N; ++i) printf("%d ", array[i]);
+        c[i] = a[i];
+        printf("%d ", a[i]);
+    }
     printf("\n");
-    for (int i=0; i<N; ++i) printf("%d ", array[i]);
-    return OK;
+
+    for (int i= 0; i< b_size; ++i)
+    {
+        
+        b[i] = rand()%2001 - 1000;
+        printf("%d ", b[i]);
+    }
+    printf("\n");
+
+
+
+    qsort(b, b_size, sizeof(int), compare);
+    for (int i= 0; i< b_size; ++i)
+    {
     
+        printf("%d ", b[i]);
+    }
+    printf("\n");
+    int l_bound, r_bound, t;
+    for (int i = 0; i< a_size; ++i)
+    {
+        l_bound =0;
+        r_bound = b_size-1;
+        while(r_bound-l_bound > 1)
+        {
+            t = (l_bound + r_bound)/2;
+            if (a[i]< b[t]) r_bound = t;
+            else l_bound = t;
+        }
+        printf("%d %d\n", a[i],b[l_bound]);
+        a[i] += b[l_bound];
+    }
+
+    for (int i= 0; i< a_size; ++i)
+    {
+    
+        printf("%d ", c[i]);
+    }
+    printf("\n");
+    free(a);
+    free(b);
+    free(c);
+    return OK;
+
 }
